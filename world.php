10c62a2df52f83d382a940cc8fc9c0e2477f776b
vdrews <vanessa.andrews876@gmail.com>
2024-11-25 03:02:53 -0500

    Exercise 4

--- world.php
+++ world.php
@@ -16,30 +16,48 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
 if($results){
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
   //print_r($results);
 
-  foreach($results as $row){
-  echo ($row['name'])."<br>";
+ // foreach($results as $row){
+  //echo ($row['name'])."<br>";
  
-  }
+  //}
 }
 else{
   echo("ERROR");
 }
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
- 
-
-
-/*
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>*/
+
+<?php if($results): ?>
+ <table>
+  <tr>
+    <th>Country Name</th>
+    <th> Continent</th>
+    <th>Independence year</th>
+    <th>Head of state</th>
+
+  </tr>
+  
+  <?php foreach($results as $row): ?>
+
+    <tr>
+      <th><?= $row['name']?></th>
+      <th><?= $row['continent']?></th>
+      <th><?= $row['independence_year']?></th>
+      <th><?= $row['head_of_state']?></th>
+
+    </tr>
+
+    <?php endforeach; ?>
+ </table>
+
+ <?php elseif(!$results): ?>
+
+  <?php endif; ?>
+
+
+
