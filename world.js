ea465f55f268b08c668e6d884561f92071dd4a29
vdrews <vanessa.andrews876@gmail.com>
2024-11-25 20:01:00 -0500

    Exercise 4

--- world.js
+++ world.js
@@ -1,37 +1,150 @@
 document.addEventListener('DOMContentLoaded', function()
 {
 
-   const lookupbtn = document.getElementById("lookup"); 
-    //listening for lookupbtn being pressed 
+   const lookupcountry = document.getElementById("lookup"); 
+   const lookupcity = document.getElementById("city");
 
+    //listening for lookupbtn being pressed 
+   // const lookupcity = document.getElementById("lookup-city"); 
+
+    lookupcountry.addEventListener("click", function(e){
+        e.preventDefault();
+        btnclicked(e);
+
+    });
+
+
+   lookupcity.addEventListener("click", function(e){
+        e.preventDefault();
+        btnclicked(e);
+    });
+
+
+    function btnclicked(event){
+        var  clickedBtn = event.target;
+        const xhr = new XMLHttpRequest();
+
+        if(clickedBtn.id == "lookup"){
+                const input = document.getElementById('country').value;
+            
+                var url = "world.php?country="+ encodeURIComponent(input) + "&city=false";
+            
+                xhr.open('GET', url, true);
+            // xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded' );
+
+                xhr.onload = function(){
+                    if(this.status == 200){
+                    result.innerHTML = this.responseText;
+                    console.log(result.innerHTML);
+                    }
+                }
+                xhr.onerror = function(){
+                    console.log('request error ');
+                }
+            
+                xhr.send();
+
+        console.log("a country yah look");
+
+
+        }
+        else if(clickedBtn.id == "city") {
+            console.log("a city yah look");
+
+            const input = document.getElementById('country').value;
+            
+            var url = "world.php?country="+ encodeURIComponent(input) + "&city=true";
+        
+            xhr.open('GET', url, true);
+        // xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded' );
+
+            xhr.onload = function(){
+                if(this.status == 200){
+                result.innerHTML = this.responseText;
+                console.log(result.innerHTML);
+                }
+            }
+            xhr.onerror = function(){
+                console.log('request error ');
+            }
+        
+            xhr.send();
+        }
+
+
+    }
+
+
+
+
+
+
+/*
+
+
+working code
     const xhr = new XMLHttpRequest();
 
-    console.log(xhr)
-    console.log("FUNCTIONAL")
-
-
-    lookupbtn.addEventListener("click", function(e){
-
+    lookupcountry.addEventListener("click", function(e){
         e.preventDefault();
         const input = document.getElementById('country').value;
-
-        var url = "world.php?country="+ encodeURIComponent(input);
+      
+        var url = "world.php?country="+ encodeURIComponent(input)
      
-    xhr.open('GET', url, true);
-    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded' );
+        xhr.open('GET', url, true);
+       // xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded' );
 
-    xhr.onload = function(){
-        if(this.status == 200){
-           result.innerHTML = this.responseText;
-         console.log(result.innerHTML);
+        xhr.onload = function(){
+            if(this.status == 200){
+            result.innerHTML = this.responseText;
+            console.log(result.innerHTML);
+            }
         }
-    }
-    xhr.onerror = function(){
-        console.log('request error ');
-    }
-   
-    xhr.send();
-    })
+        xhr.onerror = function(){
+            console.log('request error ');
+        }
+    
+        xhr.send();
+
+
+    });*/
+
+//console.log("the other button slot");
+///city button 
+
+//console.log(xhr2)
+//console.log("nonFUNCTIONAL")
+/*
+   lookupcity.addEventListener("click", function(element){
+        console.log("from city to cityyyy");
+
+        element.preventDefault();
+        const input = document.getElementById('country').value;
+
+        
+
+        var url = "world.php?country="+ encodeURIComponent(input) + "&lookup =" + encodeURIComponent(input);
+     
+        xhr.open('GET', url, true);
+       // xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded' );
+
+        xhr.onload = function(){
+            if(this.status == 200){
+            result.innerHTML = this.responseText;
+            console.log(result.innerHTML);
+            }
+        }
+        xhr.onerror = function(){
+            console.log('request error ');
+        }
+    
+        xhr.send();
+
+  });
+*/
+
+
+
 
 
 })
--- world.php
+++ world.php
@@ -7,32 +7,46 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-$country = $_GET['country']?? '';
 
+$country = $_GET['country']?? '';
+$city = $_GET['city'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-//$stmt = $conn->query("SELECT * FROM countries");
+//$stmt= $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+//$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+if($city == "false"){
+  $stmt= $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
+
+elseif($city=="true"){
+  $stmt = $conn->query("SELECT c.id, c.name AS city , c.district , c.country_code, cs.name, c.population FROM cities c JOIN countries cs ON c.country_code = cs.code WHERE cs.name LIKE '%$country%'  ");
+
+
+}
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+/*
 
 if($results){
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-  //print_r($results);
+  $stmt = $conn->query("SELECT c.name as city,c.district, c.population FROM cities c JOIN countries cs on c.country_code = cs.code WHERE c.population;");
 
- // foreach($results as $row){
-  //echo ($row['name'])."<br>";
- 
-  //}
+
+
 }
 else{
-  echo("ERROR");
+  $stmt = $conn->query("SELECT c.name as city,c.district, c.population FROM cities c JOIN countries cs on c.country_code = cs.code WHERE c.population;");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  //echo("ERROR");
 }
+
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+*/
+
 ?>
 
 
-<?php if($results): ?>
+<?php if($city == "false"): ?>
  <table>
   <tr>
     <th>Country Name</th>
@@ -55,7 +69,26 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <?php endforeach; ?>
  </table>
 
- <?php elseif(!$results): ?>
+ <?php elseif($city=="true"): ?>
+  <table>
+  <tr>
+    <th> Name</th>
+    <th> District</th>
+    <th>Population</th>
+    
+  </tr>
+  
+  <?php foreach($results as $row): ?>
+
+    <tr>
+      <th><?= $row['name']?></th>
+      <th><?= $row['district']?></th>
+      <th><?= $row['population']?></th>
+      
+    </tr>
+
+    <?php endforeach; ?>
+ </table>
 
   <?php endif; ?>
 
